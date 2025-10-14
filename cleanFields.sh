103407eaca9be3f3c66fb4cb4f0f44ab1f828367
xabor66 <xabor66@gmail.com>
2025-10-14 19:46:24 -0400

    done

--- cleanFields.sh
+++ cleanFields.sh
@@ -1,16 +1,19 @@
-while IFS=",", read -r col1 col2 col3 col4 col5 col6
+#!/bin/bash
+
+while IFS=',' read -r col1 col2 col3 col4 col5 col6
 do
-    if [$col1 =~ "\d+"]; then
-		if [$col2 =~ "\w+\s\w+"]; then
-			if [$col3 =~ "/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/"]; then
-				if [$col4 -gt $0]; then
-					if [$col5 =~ "\w+\s*\w+"]; then
-						if [$col6 =~ \w+]; then
-							printf "$col1,$col2,$col3,$col4,$col5,$col6" >> result.csv
-						fi
-					fi
-				fi
-			fi
-		fi
-	fi
-done
+    if [[ $col1 =~ ^[0-9]+$ ]]; then
+        if [[ $col2 =~ ^[A-Za-z]+[[:space:]][A-Za-z]+$ ]]; then
+            if [[ $col3 =~ ^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$ ]]; then
+                if [[ $col4 -gt 0 ]]; then
+                    if [[ $col5 =~ ^[A-Za-z]+([[:space:]]*[A-Za-z]*)$ ]]; then
+                        if [[ $col6 =~ ^[A-Za-z0-9_]+$ ]]; then
+                            printf '%s,%s,%s,%s,%s,%s\n' \
+                                "$col1" "$col2" "$col3" "$col4" "$col5" "$col6" >> result.csv
+                        fi
+                    fi
+                fi
+            fi
+        fi
+    fi
+done < input.csv
